0f776fc085f0fa583203a316ad82f830eacd9524
Sam Timmermans <sammy.timmermans@gmail.com>
2026-01-11 16:47:50 +0100

    Added embedding

--- embed.py
+++ embed.py
@@ -5,3 +5,5 @@ collection = client.get_or_create_collection("docs")
 
 with open("k8s.txt", "r") as f:
     text = f.read()
+    collection.add(documents=[text], ids=["k8s"])
+    print("Embeddings stored in chroma")
