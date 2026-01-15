a3379ca45f4669b355186244d8bf7e34d2802f6a
Sam Timmermans <sammy.timmermans@gmail.com>
2026-01-15 21:11:21 +0100

    test 2

--- semantic_test.py
+++ semantic_test.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     print("All semantic tests passed!")
 
 
-#This is a test to see if github actions is working
+#This is a test to see if github actions is working again
